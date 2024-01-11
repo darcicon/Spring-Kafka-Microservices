588bac43ff224df178a1f3d7d6ec8197420afdc3
darcicon <84118082+darcicon@users.noreply.github.com>
2024-01-11 12:11:51 +0530

    Add files via upload

---
+++ SpringBoot-Kafka-microservices/base-domains/src/main/java/com/example/basedomains/dto/Order.java
@@ -0,0 +1,18 @@
+package com.example.basedomains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    private String orderId;
+
+    private String OrderName;
+
+    private String quantity;
+
+    private double price;
+}
---
+++ SpringBoot-Kafka-microservices/email-service/src/main/java/com/example/emailService/kafka/EmailConsumer.java
@@ -0,0 +1,19 @@
+package com.example.emailService.kafka;
+
+import com.example.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmailConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}",groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent orderEvent) {
+        LOGGER.info(String.format("Event received %s",orderEvent.toString()));
+
+
+    }
+}
